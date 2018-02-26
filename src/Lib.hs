d4d18bd169eb4ca92948d77285ada3f9c89caf7e
Ibnu D. Aji <teratower8@gmail.com>
2018-02-26 22:48:36 +0700

    added first char parser. i'm sleepy.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,19 +1,38 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Lib
-  ( someFunc
-  ) where
+module Lib where
 
 import           Data.Attoparsec.Text
-import           Data.Text
+import qualified Data.Text            as T
 
--- | Consonants of https://en.wikipedia.org/wiki/Hanacaraka
--- javanese script.
-consonants :: [Text]
-consonants = [ "h", "n", "c", "r", "k"
-             , "d", "t", "s", "w", "l"
-             , "p", "dh", "j", "y", "ny"
-             , "m", "g", "b", "th", "ng"
-             ]
+-- | First part of javanese script.
+-- https://en.wikipedia.org/wiki/Hanacaraka
+consonants1 :: [T.Text]
+consonants1 = [ "h", "n", "c", "r", "k"
+              , "d", "t", "s", "w", "l"
+              ]
+
+-- | Second part of the consonants.
+consonants2 :: [T.Text]
+consonants2 = [ "p", "dh", "j", "y", "ny"
+              , "m", "g", "b", "th", "ng"
+              ]
+
+-- | Vocals "a", "i", "u", "e", and "o".
+vowels :: [Char]
+vowels = ['a', 'o', 'e', 'i', 'u']
+
+parserFirstChar :: Parser [Char]
+parserFirstChar = do
+  [v] <- count 1 digit
+  return $
+    if inClass vowels v
+      then "h" ++ [v]
+      else [v]
+
+normalizeWord :: Parser [Char]
+normalizeWord = do
+  firstcharacter <- parserFirstChar
+  return firstcharacter
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
